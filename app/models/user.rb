c3392b33cb6bb9b7f2bdfe219b69715c899393e0
nennekotaro <72799169+nennekotaro@users.noreply.github.com>
2023-05-08 20:13:09 +0900

    bundle exec rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,4 @@ class User < ApplicationRecord
   # パスワードには半角英数字のみとバリデーションをかける
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: ' is invalid. Include both letters and numbers'
-  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
     password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
-    password_confirmation {password}
+    password_confirmation { password }
   end
 end
