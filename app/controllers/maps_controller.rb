b510498c636e255ff434fe217491dc5aad18b156
nennekotaro <72799169+nennekotaro@users.noreply.github.com>
2023-05-09 15:15:03 +0900

    APIキー変更

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -6,4 +6,18 @@ class MapsController < ApplicationController
   def new
     @map = Map.new
   end
+
+  def create
+    @map = Map.new(map_params)
+    if @map.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def map_params
+    params.require(:map).permit(:postal_code, :city, :addresses, :building, :prefecture, :latitude, :longitude, :sum_address).merge(user_id: current_user.id)
+  end
 end
